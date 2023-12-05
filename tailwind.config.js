39a3387986c55b17bb0fa1297157117c6f10bd44
code-scanner <cyber.gupta001@gmail.com>
2023-12-05 18:26:44 +0530

    adjusted tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
     content: ["./**/*.{html,js}"],
     theme: {
-      extend: {},
+      
+      extend: {
+        colors : {
+          "primary" : "rgb(0 72 75)",
+          "primary-light" : "rgb(0 72 75)",
+          "primary-vlight" : "rgb(0 173 179)",
+          "primary-dark" : "rgb(0 49 51)",
+        }
+      },
+      
     },
     plugins: [],
   }
